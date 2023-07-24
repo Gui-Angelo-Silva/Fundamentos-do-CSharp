889fd7186fa1587fb4f6c95bfb20daa32c7bffe2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 09:59:15 -0300

    Aula Operadores de Atribuição - Fundamentos do C#, curso balta.io

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -6,22 +6,11 @@ namespace MeuApp
     {
         static void Main(string[] args) 
         {
-            int soma = 25 + 22;
-            int subtracao = 25 - 22;
-            int divisao = 25 * 22;
-            int multiplicacao = 22 / 5;
-
-            int x = 2 + 2 * 2;
-            int y = 2 + (2 * 2);
-            int z = (2 + 2) * 6;
-
-            Console.WriteLine(soma);
-            Console.WriteLine(subtracao);
-            Console.WriteLine(divisao);
-            Console.WriteLine(multiplicacao);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            int x = 0;
+            x += 5;
+            x -= 1;
+            x *= 10;
+            x /= 2;
         }
     }
 } 
