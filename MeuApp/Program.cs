39f3d5c47cd419375b071daf65a569f98c23e182
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 08:42:02 -0300

    Aula VAR - Fundamentos do C#, curso balta.io

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args) 
         {
-            
+            IEnumerable<MeuTipoComplexo> aluno = new IEnumerable<MeuTipoComplexo>();
+            char aluno = new IEnumerable<MeuTipoComplexo>();
         }
     }
 } 
