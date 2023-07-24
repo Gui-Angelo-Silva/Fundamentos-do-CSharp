f675a273117a9e92bf9f4ac3b5dc9acbecd81820
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 09:51:44 -0300

    Aula Convertendo Tipos - Fundamentos do C#, curso balta.io

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -6,7 +6,20 @@ namespace MeuApp
     {
         static void Main(string[] args) 
         {
-            int inteiro = Convert.ToInt32("100");
+            int inteiro = 100;
+            float real = 25.8f;
+
+            //real = inteiro; // 100.0f
+            //inteiro = (int)real;
+            //string valorReal = real.ToString();
+            //inteiro = int.Parse("255");
+            //inteiro = int.Parse(real.ToString());
+            //inteiro = Convert.ToInt32(real);
+
+            //Console.WriteLine(valorReal);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(Convert.ToBoolean("true"));
+            //Console.WriteLine(Convert.ToInt32(real));
         }
     }
 } 
