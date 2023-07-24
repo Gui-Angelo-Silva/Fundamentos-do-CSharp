27dade294b1f59e48b5f63a15468d26c49d3b8c3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 12:02:10 -0300

    Mão na Massa: Criando uma Calculadora - Fundamentos do C#, curso balta.io

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
