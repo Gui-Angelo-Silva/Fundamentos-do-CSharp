5ad4f1542a3516f97c6211594f37b8a08ee05871
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 10:59:18 -0300

    Aula Laços de Repetição: For - Fundamentos do C#, curso balta.io

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -6,13 +6,8 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            bool? valor = null;
-            switch (valor)
-            {
-                case true: Console.WriteLine("Verdadeiro!"); break;
-                case false: Console.WriteLine("Falso!"); break;
-                default: Console.WriteLine("Nulo"); break;
-            }
+           for (int i = 5; i >= 0; i--)
+            Console.WriteLine(i);
         }
     }
 }
