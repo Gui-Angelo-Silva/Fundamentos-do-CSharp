08c9f3d40bd71340e68aabfd16ea44b963641c5b
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-24 08:20:46 -0300

    Aula CHAR - Fundamentos do C#, curso balta.io

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -4,9 +4,8 @@
     {
         static void Main(string[] args) 
         {
-            bool usuarioCadastrado = false;
-            bool pagamentoRecebido = true;
-            var usuarioExpirado = false;
+            char primeiraLetra = 'C';
+            var segundaLetra = 'D';
         }
     }
 } 
