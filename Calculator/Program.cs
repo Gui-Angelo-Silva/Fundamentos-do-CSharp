ef102d12a372d37aeb762d8ea7c2b9c96143908c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-25 08:40:02 -0300

    Criando uma calculadora: Subtração, curso balta.io

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma() {
@@ -26,6 +26,23 @@ namespace Calculator
             //Console.WriteLine($"O resultado da soma é {v1 + v2}");
             //Console.WriteLine("O resultado da soma é " + (v1 + v2));
             //Console.WriteLine(v1);
+            Console.ReadKey();
+        }
+
+        static void Subtracao() {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Primeiro valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
         }
     }
 }
