b7cf3fd594396b577c76e1049f624579f8070873
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-25 08:22:03 -0300

    Criando uma calculadora: Soma, curso balta.io

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,10 +5,21 @@
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            
+            float resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            //Console.WriteLine(v1);
         }
     }
 }
