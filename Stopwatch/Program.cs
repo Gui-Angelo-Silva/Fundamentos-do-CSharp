be476b8da9af1cdc3384ade25b54ca629f7ff271
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-25 09:47:46 -0300

    Cronômetro: Thread e Sleep, curso balta.io

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stopwatch
 {
@@ -6,7 +7,7 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
         }
 
         static void Start()
@@ -15,8 +16,11 @@ namespace Stopwatch
             int currentTime = 0;
 
             while (currentTime != time)
-            {
+            {   
+                Console.Clear();
                 currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
             }
         }
     }
