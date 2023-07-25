a3802e92d9360cede1e69a86fe00866cf240c9b8
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-25 10:52:07 -0300

    Cronômetro: Calculando o tempo, curso balta.io

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -19,11 +19,17 @@ namespace Stopwatch
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            char type = char.Parse(data.Substring(data.Length - 1,1));
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
-            
-            Console.WriteLine(type);
-            Console.WriteLine(time);
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
         }
 
         static void Start(int time)
@@ -31,7 +37,7 @@ namespace Stopwatch
             int currentTime = 0;
 
             while (currentTime != time)
-            {   
+            {
                 Console.Clear();
                 currentTime++;
                 Console.WriteLine(currentTime);
@@ -41,6 +47,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
