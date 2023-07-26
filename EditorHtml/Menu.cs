99b9302b94457b66763584064df56a877e3fb9b2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-26 10:51:39 -0300

    Editor HTML: Exibindo o menu, curso balta.io

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -11,6 +11,9 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         public static void DrawScreen()
@@ -41,6 +44,24 @@ namespace EditorHtml
                 Console.Write("|");
                 Console.Write("\n");
             }
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir Arquivo");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
         }
     }
 }
