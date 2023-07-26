5ecb9626e73dc769a9f30aa3633a437564244487
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-26 11:51:07 -0300

    Datas: Formatando datas, curso balta.io

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -9,17 +9,21 @@ namespace MyApp
         {
             Console.Clear();
 
-            var data = new DateTime(2005, 07, 9, 8, 23, 14);
-            //var data = DateTime.Now;
-            Console.WriteLine(data);
-            Console.WriteLine(data.Year);
-            Console.WriteLine(data.Month);
-            Console.WriteLine(data.Day);
-            Console.WriteLine(data.Hour);
-            Console.WriteLine(data.Minute);
-            Console.WriteLine(data.Second);
+            //var data = new DateTime(2005, 07, 9, 8, 23, 14);
+            var data = DateTime.Now;
+            
+            // 
+            var formatada = String.Format("{0:dd/MM/yyyy hh:mm:ss ff z}", data);
+            Console.WriteLine(formatada);
+            // Console.WriteLine(data);
+            // Console.WriteLine(data.Year);
+            // Console.WriteLine(data.Month);
+            // Console.WriteLine(data.Day);
+            // Console.WriteLine(data.Hour);
+            // Console.WriteLine(data.Minute);
+            // Console.WriteLine(data.Second);
 
-            Console.WriteLine(data.DayOfWeek);
+            // Console.WriteLine(data.DayOfWeek);
         }
     }
 }
