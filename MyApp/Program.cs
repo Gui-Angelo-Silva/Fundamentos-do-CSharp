a3cfb3467f251ad6076af6f68f3651f3fac290fb
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-26 09:28:12 -0300

    String: Métodos adicionais, curso balta.io

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,8 +7,11 @@ namespace MyApp
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.IndexOf("é"));
-            Console.WriteLine(texto.LastIndexOf("s"));
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.ToUpper());
+            Console.WriteLine(texto.Insert(5, "AQUI "));
+            Console.WriteLine(texto.Remove(5, 5));
+            Console.WriteLine(texto.Length);
         }
     }
 }
