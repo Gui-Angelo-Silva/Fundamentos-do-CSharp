5075bcb24145536efe92689facae273c0c2388ae
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-26 10:41:32 -0300

    Editor HTML: Desenhando a tela, curso balta.io

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -10,7 +10,37 @@ namespace EditorHtml
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            ColumnScreen();
+            LineScreen();
+            ColumnScreen();
+        }
+
+        public static void ColumnScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        public static void LineScreen()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
         }
     }
 }
