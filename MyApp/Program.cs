32bd5c13e4b0e0fb64982d6fc0f87ca86a8f56b3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-26 08:53:19 -0300

    String: Equals, curso balta.io

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,13 +7,9 @@ namespace MyApp
         static void Main(string[] args)
         {
             var texto = "Este texto é um teste";
-            Console.WriteLine(texto.StartsWith("Este"));
-            Console.WriteLine(texto.StartsWith("este"));
-            Console.WriteLine(texto.StartsWith("texto"));
-
-            Console.WriteLine(texto.EndsWith("Este texto é um teste"));
-            Console.WriteLine(texto.EndsWith("Testo"));
-            Console.WriteLine(texto.EndsWith("xpro"));
+            Console.WriteLine(texto.Equals("Este texto é um teste"));
+            Console.WriteLine(texto.Equals("este texto é um teste"));
+            Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
         }
     }
 }
