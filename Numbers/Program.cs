9b21bba9667243a19b95ecf609a95e0bf0d6d60a
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-27 09:49:37 -0300

    Moedas: Tipo para moedas, curso balta.io

---
+++ Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.25m;
+            Console.WriteLine(valor);
+        }
+    }
+}
