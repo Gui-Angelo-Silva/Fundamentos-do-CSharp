3da6851229c206d49d4aefa895c3eb3f38e60df4
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-27 09:39:09 -0300

    Datas: Revisão, curso balta.io

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -10,26 +10,9 @@ namespace MyApp
         {
             Console.Clear();
 
-            var utcDate = DateTime.UtcNow;
-            
-            var timeSpan = new TimeSpan();
-            Console.WriteLine(timeSpan);
-
-            var timeSpanNanoSegundos = new TimeSpan(1);
-            Console.WriteLine(timeSpanNanoSegundos);
-
-            var timeSpanHoraMinutoSegundo = new TimeSpan(5, 12, 8);
-            Console.WriteLine(timeSpanHoraMinutoSegundo);
-            
-            var timeSpanDiaHoraMinutoSegundo = new TimeSpan(3, 5, 50, 18);
-            Console.WriteLine(timeSpanDiaHoraMinutoSegundo);
-
-            var timeSpanHoraMinutoSegundoMilisegundo = new TimeSpan(15, 12, 55, 8, 100);
-            Console.WriteLine(timeSpanHoraMinutoSegundoMilisegundo);
-
-            Console.WriteLine(timeSpanHoraMinutoSegundo - timeSpanDiaHoraMinutoSegundo);
-            Console.WriteLine(timeSpanDiaHoraMinutoSegundo.Days);
-            Console.WriteLine(timeSpanDiaHoraMinutoSegundo.Add(new TimeSpan(12, 0, 0)));
+            Console.WriteLine(DateTime.DaysInMonth(2020, 2));
+            Console.WriteLine(IsWeekend(DateTime.Now.DayOfWeek));
+            Console.WriteLine(DateTime.Now.IsDaylightSavingTime());
 
             //var formatada = String.Format("{0:r}", data);
             //var data = new DateTime(2005, 07, 9, 8, 23, 14);
@@ -42,6 +25,11 @@ namespace MyApp
             // Console.WriteLine(data.Second);
 
             // Console.WriteLine(data.DayOfWeek);
+        }
+
+        static bool IsWeekend(DayOfWeek today)
+        {
+            return today == DayOfWeek.Saturday || today == DayOfWeek.Sunday;
         }
     }
 }
