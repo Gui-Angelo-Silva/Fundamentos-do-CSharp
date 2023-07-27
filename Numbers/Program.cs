378d0e33f9ffd5e7e9a2bb47a14dd27ae82084b1
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-27 10:05:47 -0300

    Moedas: Math, curso balta.io

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -10,12 +10,9 @@ namespace Numbers
             Console.Clear();
 
             decimal valor = 10.25m;
-            Console.WriteLine(
-                valor.ToString(
-                    "C",
-                    CultureInfo.CreateSpecificCulture("pt-BR")
-                )
-            );
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
         }
     }
 }
