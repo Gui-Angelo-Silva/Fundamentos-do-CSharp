f9ad4bef38ab3f275c4e5511b31915a2e111c7ff
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-28 09:04:09 -0300

    Exceptions: Disparando exceções, curso balta.io

--- DotnetLists/Program.cs
+++ DotnetLists/Program.cs
@@ -9,11 +9,13 @@ namespace DotnetLists
             var arr = new int[3];
             try
             {
-                for (var index = 0; index < 10; index++)
-                {
-                    //IndexOutOfRangeException
-                    Console.WriteLine(arr[index]);
-                }
+                // for (var index = 0; index < 10; index++)
+                // {
+                //     //IndexOutOfRangeException
+                //     Console.WriteLine(arr[index]);
+                // }
+                
+                Cadastrar("");
             }
             catch (IndexOutOfRangeException ex)
             {
@@ -21,12 +23,24 @@ namespace DotnetLists
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Não encontrei o índice na lista");
             }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Falha ao cadastrar texto");
+            }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("Ops, algo deu errado!");
             }
+        }
+
+        static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
         }
     }
 }
